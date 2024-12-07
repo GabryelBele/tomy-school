c18f9237da8152057a516c179081edf623528174
GabryelBele <belegabryel19@gmail.com>
2024-12-07 17:53:33 -0300

    Commit Initial

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.tsx'
+import GlobalStyled from './styles/GlobalStyled.ts'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <GlobalStyled>
+      <App />
+    </GlobalStyled>
+  </StrictMode>,
+)
---
+++ src/styles/GlobalStyled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const GlobalStyled = styled.div`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    body {
+        color: #FFFFFF;
+    }`;
+
+export default GlobalStyled;
